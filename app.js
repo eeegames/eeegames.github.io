540740f79de55cc4cda6c1ca6284eb4720c575e1
eeegames <86306797+eeegames@users.noreply.github.com>
2021-06-22 23:29:54 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict'
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/main.html');
+})
+
+app.use(express.static(__dirname + '/app-ads'));
+
+app.listen(port);
