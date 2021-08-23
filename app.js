5bb18bd7390a14e2c006b634c4bad17597e0457e
eeegames <86306797+eeegames@users.noreply.github.com>
2021-08-24 00:53:40 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,30 +8,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-const langs = ['ko', 'en'];
-const apps = ['gyedole', 'kudole', 'gongdole', 'musicgenerator', 'screenblacker', 'ballracer'];
-for (var i = 0; i < langs.length; i++) {
-    /*var p = '/' + langs[i];
-    app.get(p, (req, res) => {
-        res.sendFile(__dirname + '/index.html');
-    });*/
-    /*
-    p += '/apps';
-    app.get(p, (req, res) => {
-        res.sendFile(__dirname + '/apps/appList.html');
-    });
-    p += '/';
-    for (var j = 0; j < apps.length; j++) {
-        app.get(p + apps[j], (req, res) => {
-            res.sendFile(__dirname + '/apps/' + apps[j] + '/info.html');
-        });
-        app.get(p + apps[j] + '/privacy', (req, res) => {
-            res.sendFile(__dirname + '/apps/' + apps[j] + '/privacy.html');
-        });
-    }
-    */
-}
-
 app.use(express.static(__dirname + '/app-ads.txt'));
 
 app.listen(port);
