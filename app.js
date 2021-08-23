0d3075bbcd749bcd93b7bbd857d2ac7affeb5785
eeegames <86306797+eeegames@users.noreply.github.com>
2021-08-23 18:34:24 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-app.use(express.static(__dirname + '/app-ads.txt'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port);
